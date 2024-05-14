13ebde5f70b35e486b43c5478cc47b5071829b31
Thomasflantu <thomas.flantua@gmail.com>
2024-05-14 10:54:45 +0200

    Changed mainsection for own information, the github link isn't showing yet, because i need to do some changes into my github

--- src/data/sections/main-section.data.ts
+++ src/data/sections/main-section.data.ts
@@ -1,6 +1,9 @@
 import type { MainSection } from '@/types/sections/main-section.types';
 import type { ReadonlyDeep } from 'type-fest';
-import { facebook, github, linkedin, twitter } from '../helpers/links';
+import { facebook, github, instagram, linkedin, twitter } from '../helpers/links';
+
+const phoneNumber = '+31633089143'
+const emailAddress = 'thomas.flantua@gmail.com'
 
 const mainSectionData = {
   config: {
@@ -10,30 +13,30 @@ const mainSectionData = {
     visible: true,
   },
   image: import('@/assets/my-image.jpeg'),
-  fullName: 'Mark Freeman',
-  role: 'Senior React Developer',
+  fullName: 'Thomas Flantua',
+  role: 'Student Technische Informatica aan Breda Avans',
   details: [
-    { label: 'Phone', value: '605 475 6961', url: 'tel:605 475 6961' },
-    { label: 'Email', value: 'mark.freeman.dev@gmail.com', url: 'mailto:mark.freeman.dev@gmail.com' },
-    { label: 'From', value: 'Warsaw, Poland' },
-    { label: 'Salary range', value: '18 000 - 25 000 PLN' },
+    { label: 'Phone', value: phoneNumber, url: 'tel:' + phoneNumber },
+    { label: 'Email', value: emailAddress, url: 'mailto:' + emailAddress },
+    { label: 'From', value: 'Dordrecht, Nederland' },
   ],
   pdfDetails: [
-    { label: 'Phone', value: '605 475 6961' },
-    { label: 'Email', value: 'mark.freeman.dev@gmail.com' },
-    { label: 'LinkedIn', value: '/in/mark-freeman', url: 'https://linkedin.com' },
-    { label: 'GitHub', value: '/mark-freeman', url: 'https://github.com' },
-    { label: 'Website', value: 'mark-freeman-personal-website.com', url: '/', fullRow: true },
+    { label: 'Phone', value: phoneNumber },
+    { label: 'Email', value: emailAddress },
+    { label: 'LinkedIn', value: '/in/thomas-flantua-3b543517b', url: 'https://linkedin.com' },
+    { label: 'Website', value: 'https://euphonious-queijadas-62a72f.netlify.app/#profile', url: '/', fullRow: true },
   ],
   description:
-    'Lorem ipsum dolor sit amet, consectetur **adipiscing elit**. In sodales ac dui at *vestibulum*. In condimentum metus id dui tincidunt, in blandit mi [vehicula](/). Nulla lacinia, erat sit amet elementum vulputate, lectus mauris volutpat mi, vitae accumsan metus elit ut nunc. Vestibulum lacinia enim eget eros fermentum scelerisque. Proin augue leo, posuere ut imperdiet vitae, fermentum eu ipsum. Sed sed neque sagittis, posuere urna nec, commodo leo. Pellentesque posuere justo vitae massa volutpat maximus.',
-  tags: [{ name: 'Open for freelance' }, { name: 'Available for mentoring' }, { name: 'Working on side project' }],
+  'Een hardwerkende student. Wilt zijn vaardigheden en passie inzetten om de missie van een onderneming vorm te geven. ' + 
+  ' Bedreven in het omgaan met technologie, waaronder diverse sociaal-mediaplatforms en office-programma’s. ' + 
+  'Beschikt over gevorderde computervaardigheden. Heeft een positieve attitude en is bereid en gemotiveerd om nieuwe programma’s te leren beheersen.',
+ // tags: [{ name: 'Open for freelance' }, { name: 'Available for mentoring' }, { name: 'Working on side project' }],
   action: {
     label: 'Download CV',
     url: '/cv.pdf',
-    downloadedFileName: 'CV-Mark_Freeman.pdf',
+    downloadedFileName: 'CV-Thomas_Flantua.pdf',
   },
-  links: [facebook({ url: '#' }), github({ url: '#' }), linkedin({ url: '#' }), twitter({ url: '#' })],
+  links: [instagram({ url: 'https://www.instagram.com/thomasf_078/?next=%2F' }), linkedin({ url: 'https://www.linkedin.com/in/thomas-flantua-3b543517b/' })],
 } as const satisfies ReadonlyDeep<MainSection>;
 
 export default mainSectionData;
