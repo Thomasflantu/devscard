8cfe96227aaea63597608e36df65a143ed361abc
Thomasflantu <thomas.flantua@gmail.com>
2024-05-14 10:31:20 +0200

    First commig

--- src/data/config.ts
+++ src/data/config.ts
@@ -11,7 +11,7 @@ const config = {
     },
   },
   meta: {
-    title: 'Mark Freeman - Senior React Developer',
+    title: 'Thomas Flantua - Student Technische informatie Breda Avans',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales ac dui at vestibulum. In condimentum metus id dui tincidunt, in blandit mi vehicula.',
     faviconPath: '/src/assets/my-image.jpeg',
