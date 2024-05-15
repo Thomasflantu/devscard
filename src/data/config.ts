aeceab35860e024f09d17e6c2e01e199cb89ba30
Thomasflantu <thomas.flantua@gmail.com>
2024-05-15 14:27:21 +0200

    Changed headerphoto

--- src/data/config.ts
+++ src/data/config.ts
@@ -16,7 +16,7 @@ const config = {
       'Een hardwerkende student. Wilt zijn vaardigheden en passie inzetten om de missie van een onderneming vorm te geven. ' + 
       ' Bedreven in het omgaan met technologie, waaronder diverse sociaal-mediaplatforms en office-programma’s. ' + 
       'Beschikt over gevorderde computervaardigheden. Heeft een positieve attitude en is bereid en gemotiveerd om nieuwe programma’s te leren beheersen.',
-    faviconPath: '/src/assets/my-image.jpeg',
+    faviconPath: '/src/assets/HeadImage_CV.jpg',
   },
   pdf: {
     footer:
