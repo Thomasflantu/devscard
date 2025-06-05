d8ded0c6035de438061bf12162c45fbc2abb58ae
Thomas Flantua <t.flantua@student.avans.nl>
2025-06-05 12:25:21 +0200

    Adds object-cover class to Photo component
    Ensures images in the Photo component maintain their aspect ratio and cover the entire container by applying the `object-cover` class.
    This also adds default class support to the Photo component.
    
    Removes Instagram link.

--- src/data/sections/main-section.data.ts
+++ src/data/sections/main-section.data.ts
@@ -24,8 +24,8 @@ const mainSectionData = {
     { label: 'Website', value: 'https://euphonious-queijadas-62a72f.netlify.app/#profile', url: 'https://euphonious-queijadas-62a72f.netlify.app/#profile', fullRow: true },
   ],
   description:
-  'Een hardwerkende student. Wilt zijn vaardigheden en passie inzetten om de missie van een onderneming vorm te geven. ' + 
-  ' Bedreven in het omgaan met technologie, waaronder diverse sociaal-mediaplatforms en office-programma’s. ' + 
+  'Een hardwerkende student. Wilt zijn vaardigheden en passie inzetten om de missie van een onderneming vorm te geven. ' +
+  ' Bedreven in het omgaan met technologie, waaronder diverse sociaal-mediaplatforms en office-programma’s. ' +
   'Beschikt over gevorderde computervaardigheden. Heeft een positieve attitude en is bereid en gemotiveerd om nieuwe programma’s te leren beheersen.',
   tags: [],
   action: {
@@ -33,7 +33,7 @@ const mainSectionData = {
     url: '/cv.pdf',
     downloadedFileName: 'CV-Thomas_Flantua.pdf',
   },
-  links: [instagram({ url: 'https://www.instagram.com/thomasf_078/?next=%2F' }), linkedin({ url: 'https://www.linkedin.com/in/thomas-flantua-3b543517b/' })],
+  links: [linkedin({ url: 'https://www.linkedin.com/in/thomas-flantua-3b543517b/' })],
 } as const satisfies ReadonlyDeep<MainSection>;
 
 export default mainSectionData;
