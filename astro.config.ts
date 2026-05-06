240e771581f3e2321bca6a664f150af54c3a07ea
Thomas Flantua <t.flantua@optimation.nl>
2026-05-06 17:00:17 +0200

    chore: Remove unused Astro integrations and dependencies
    This commit cleans up the project by removing several Astro integrations and their associated packages that are no longer necessary.
    
    The `@astrojs/image` integration is removed, completing the migration to `astro:assets`. Additionally, `@astrojs/tailwind` and `@astrojs/react` are removed as they are no longer utilized in the project.

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
-import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [compress()],
   vite: {
     plugins: [visualizer()],
   },
